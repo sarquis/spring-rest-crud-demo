74b3759ea2e9cbb5f67569dd3573deb4af129ae8
rsnf <roberto.sarquis@gmail.com>
2023-07-21 22:00:52 -0300

    Custom Exception Handler New custom error response class; New custrom exception class; Add @ExceptionHandler;

---
+++ spring-rest-crud/src/main/java/com/sqs/spring/restcrud/rest/StudentNotFoundException.java
@@ -0,0 +1,19 @@
+package com.sqs.spring.restcrud.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public StudentNotFoundException(String message, Throwable cause) {
+	super(message, cause);
+    }
+
+    public StudentNotFoundException(String message) {
+	super(message);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+	super(cause);
+    }
+
+}
